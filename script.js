fddc5a11580f4fd741b7232b017c454abd7d852b
pedro <pedrolucasgonza31@gmail.com>
2025-04-06 18:23:34 -0300

    up

--- script.js
+++ script.js
@@ -4,5 +4,17 @@ if (idade >= 18){
   alert("Você  pode entrar")
 } else {
   alert ("Pode não entrar")
+}
+
+var jogo = prompt ("Pedra Papel ou Tesoura?")
+if (jogo == "Pedra"){
+  alert ("Você escolheu a Pedra")
+}else if (jogo == "Papel"){
+  alert ("Você escolheru o Papel")
+
+} else if (jogo == "Tesoura"){
+  alert ("Você escolher a Tesoura")
+}else {
+  alert ("Escolha entre Pedra Pepel ou Tesoura")
 
 }
