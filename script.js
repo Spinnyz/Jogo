451f2acd662e6d3365a207c0a300da7f23db253e
pedro <pedrolucasgonza31@gmail.com>
2025-04-06 19:08:46 -0300

    up

--- script.js
+++ script.js
@@ -17,32 +17,41 @@ if (jogo == "Pedra"){
   alert ("Você escolheu a Tesoura")
 }else {
   alert ("Escolha entre Pedra Papel ou Tesoura")
+ 
 }
+
 
 //Resposta aleatoria
 var computador = Math.floor(Math.random()*3)+1;
- if (computador == 1){
+ if (computador === 1){
   computador = "Pedra"
- }else if (computador == 2){
+ }else if (computador === 2){
   computador = "Papel"
-}else if (computador == 3){
+}else if (computador === 3){
   computador = "Tesoura"
 } 
 
 //Batalha
-if (computador == jogo){
-  alert ("Empate") 
-}  else if (computador == "Pedra" && jogo == "Tesoura"){
-  alert (`Você ganhou! o computador escolheu ${computador} e você ${jogo}`)
-} else if (computador == "Pedra" && jogo == "Papel"){
-  alert (`Você perdeu! o computador escolheu ${computador} e você ${jogo}`)
-} else if (computador == "Papel" && jogo == "Pedra"){
-  alert (`Você ganhou o computador escolher ${computador} e você ${jogo}`)
-} else if (computador == "Papel" && jogo == "Tesoura"){
-  alert (`Você perdeu! o computador escolheu ${computador} e você ${jogo}`)
-} else if (computador == "Tesoura" && jogo == "Pedra"){
-  alert (`Você ganhou o computador escolher ${computador} e você ${jogo}`)
-} else if (computador == "Tesoura" && jogo == "Papel"){
-  alert (`Você perdeu! o computador escolheu ${computador} e você ${jogo}`)
-}
 
+//empate
+if (computador === jogo) {
+  alert("Empate!");
+
+//Pedra
+} else if (computador === "Pedra" && jogo === "Tesoura") {
+  alert(`Você perdeu! O computador escolheu ${computador} e você ${jogo}`);
+} else if (computador === "Pedra" && jogo === "Papel") {
+  alert(`Você ganhou! O computador escolheu ${computador} e você ${jogo}`);
+
+ //Papel 
+} else if (computador === "Papel" && jogo === "Pedra") {
+  alert(`Você perdeu! O computador escolheu ${computador} e você ${jogo}`);
+} else if (computador === "Papel" && jogo === "Tesoura") {
+  alert(`Você ganhou! O computador escolheu ${computador} e você ${jogo}`);
+
+//Tesoura
+} else if (computador === "Tesoura" && jogo === "Pedra") {
+  alert(`Você ganhou! O computador escolheu ${computador} e você ${jogo}`);
+} else if (computador === "Tesoura" && jogo === "Papel") {
+  alert(`Você perdeu! O computador escolheu ${computador} e você ${jogo}`);
+}
