ae4f03f02175bfc31fa5854a02ef459010bc34e5
pedro <pedrolucasgonza31@gmail.com>
2025-04-07 20:18:25 -0300

    up

--- calmacalebreso.js
+++ calmacalebreso.js
@@ -4,12 +4,15 @@ while (rodada <=3){
 var jogador = prompt (`Bem vindo a rodada ${rodada++} Escolha seu piso (1,2,3)`)
 var piso = Math.floor(Math.random()*3)+1;
 if (Number(jogador) === piso){
-     alert ("Você caiu")
+     alert ("Você caiu ")
 } else {
-     alert ("Você não caiu")
+     alert (`Você não caiu o piso quebrado era o ${piso}`)
 }
+rodada=1000
 
-rodada = 10000;
+if (rodada === 4){
+     alert ("Você venceu")
+}
 }
 
    
