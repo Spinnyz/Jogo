23982a7ed67121b63a0bb489a965097d08f3a224
pedro <pedrolucasgonza31@gmail.com>
2025-04-07 19:44:13 -0300

    up

--- calmacalebreso.js
+++ calmacalebreso.js
@@ -1,6 +1,8 @@
 var rodada = 1
 
-while (rodada 3){
-    console.log (`Rodade: ${rodada}`)
-    rodada =+ 1
+while (rodada <=3){
+     console.log (`Rodada: ${rodada}`)
+     
+var jogador = prompt (`Bem vindo a rodada ${rodada} Escolha seu piso (1,2,3)`)
 }
+   
--- node_dc803e8e910a9.tmp.js
+++
@@ -1,6 +0,0 @@
-var rodada = 1
-
-while (rodada <=3){
-    console.log (`Rodade: ${rodada}`)
-    rodada =+ 1
-}
